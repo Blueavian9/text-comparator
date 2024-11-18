7adb133fadde5743a056951dc1791b1578621290
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 11:55:13 -0800

    updated commit

--- text-comparator/vite.config.js
+++ text-comparator/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-
-// Vite will automatically load environment variables with the VITE_ prefix
 
 export default defineConfig({
   plugins: [react()],
-  server: {},
+  // Remove the HTTPS configuration as it's not needed for local development
+  server: {
+    https: false,
+  },
 });
