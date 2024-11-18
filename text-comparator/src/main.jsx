050563f23b51ee71fa6075b71def0040e5d5e773
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 13:43:00 -0800

    updated main.jsx and deleted caches and updated node_modules and package-lock.json files

--- text-comparator/src/main.jsx
+++ text-comparator/src/main.jsx
@@ -6,7 +6,8 @@ import App from "./App.jsx";
 const apiKey = import.meta.env.VITE_API_KEY;
 console.log(apiKey); // You should see the API key logged in the browser's console
 
-createRoot(document.getElementById("app")).render(
+const root = createRoot(document.getElementById("app"));
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
--- text-comparator/vite.config.js
+++ text-comparator/vite.config.js
@@ -5,6 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Remove the HTTPS configuration as it's not needed for local development
   server: {
-    https: false,
   },
 });
