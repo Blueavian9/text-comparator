b56f221a87e00bff83d3688da83b93677f466d75
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 13:56:00 -0800

    updated main.jsx file

--- text-comparator/src/main.jsx
+++ text-comparator/src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
-
-const apiKey = import.meta.env.VITE_API_KEY;
-console.log(apiKey); // You should see the API key logged in the browser's console
 
 const root = createRoot(document.getElementById("app"));
 root.render(
