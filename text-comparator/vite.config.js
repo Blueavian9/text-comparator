f2f7d78a47785ccc6cc2a3ba1d827e46a852dfac
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 11:35:22 -0800

    updated API-Key and Best Practices

--- text-comparator/vite.config.js
+++ text-comparator/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load .env file
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync("./key.pem"),
+      cert: fs.readFileSync("./cert.pem"),
     },
   },
 });
