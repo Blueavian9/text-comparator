b9701ea407ee801f1277990d52c7d2ceff74dbb2
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 13:14:34 -0800

    updated main.jsx

--- text-comparator/src/main.jsx
+++ text-comparator/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 console.log(apiKey); // You should see the API key logged in the console
+
 createRoot(document.getElementById("app")).render(
   <StrictMode>
     <App />
