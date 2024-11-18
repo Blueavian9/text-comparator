ef992219743f60b31b65617e4795d4f6fdea40c2
Cesar Aguilar <Blueavian9@gmail.com>
2024-11-18 11:48:11 -0800

    updated vite.config.js file

--- text-comparator/vite.config.js
+++ text-comparator/vite.config.js
@@ -6,10 +6,5 @@ import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("./key.pem"),
-      cert: fs.readFileSync("./cert.pem"),
-    },
-  },
+  server: {},
 });
